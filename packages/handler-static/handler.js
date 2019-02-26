b4af5325c388e21a9bba4c9305c9a61693c73578
Asad Memon <asad@remoteinterview.io>
2019-02-26 09:28:23 -0800

    fix security bug in static handler

--- packages/handler-static/handler.js
+++ packages/handler-static/handler.js
@@ -1,6 +1,13 @@
 const path = require('path')
 const url = require('url')
 module.exports = (request, response) => {
+  var file = path.join(process.env.BUILDPATH, url.parse(request.url).pathname)
 
-  response.sendFile(path.join(process.env.BUILDPATH, url.parse(request.url).pathname));
+  // prevent sending files outside project folder.
+  if (!require('path').relative(process.env.BUILDPATH, file).startsWith("..") ){
+    response.sendFile(file);
+  }
+  else{
+    response.sendStatus(404)
+  }
 }
