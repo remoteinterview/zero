49aa604fce7605ff896af2e565066e17e72e243e
Ademílson F. Tonato <contato@admcode.com.br>
2019-03-05 17:22:04 +0000

    chore: Update jest version to fix vulnerabilities and change jest flag (#32)
    I updated jest flag because `setupTestFrameworkScriptFile`is deprecated
    in favor of `setupFilesAfterEnv`.
    
    More details available on JestJS docs, check this out:
    https://jestjs.io/docs/en/configuration#setupfilesafterenv-array

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,13 @@
 // https://jestjs.io/docs/en/configuration.html
 
 module.exports = {
-  rootDir: 'test',  
-  testEnvironment: "node",
-  testMatch: [ "**/test/integration/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)" ],
+  rootDir: 'test',
+  testEnvironment: 'node',
+  testMatch: [
+    '**/test/integration/**/*.[jt]s?(x)',
+    '**/?(*.)+(spec|test).[jt]s?(x)'
+  ],
   globalSetup: '<rootDir>/jest-global-setup.js',
   globalTeardown: '<rootDir>/jest-global-teardown.js',
-  setupTestFrameworkScriptFile: '<rootDir>/jest.setup.js'
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js']
 };
