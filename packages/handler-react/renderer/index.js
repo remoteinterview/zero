32b7818fa1a64f04f550c5dc2bbbfd46992e5ce0
Asad Memon <asad@remoteinterview.io>
2019-03-05 09:34:25 -0800

    warn about ssr crash only once

--- packages/handler-react/renderer/index.js
+++ packages/handler-react/renderer/index.js
@@ -1,133 +1,160 @@
-require("@babel/polyfill")
-require("./mdx-override") // convert mdx to jsx on import()
-const babelConfig = require("./babel.config")
-require('@babel/register')({
-  extensions: ['.js', '.jsx', '.tsx', '.ts'], 
+require("@babel/polyfill");
+require("./mdx-override"); // convert mdx to jsx on import()
+const babelConfig = require("./babel.config");
+require("@babel/register")({
+  extensions: [".js", ".jsx", ".tsx", ".ts"],
   ...babelConfig
-})
-require('ignore-styles') // ignore css/scss imports on server side.
-const ISDEV = process.env.NODE_ENV!=="production"
-const debug = require('debug')('react')
-const fs = require('fs')
-const path = require('path')
-const React = require('react')
-const {
-  renderToString
-} = require('react-dom/server')
+});
+require("ignore-styles"); // ignore css/scss imports on server side.
+const ISDEV = process.env.NODE_ENV !== "production";
+const debug = require("debug")("react");
+const fs = require("fs");
+const path = require("path");
+const React = require("react");
+const { renderToString } = require("react-dom/server");
 
 // we use client's helmet instance to avoid two Helmet instances to be loaded.
 // see: https://github.com/nfl/react-helmet/issues/125
 // and https://stackoverflow.com/questions/45822925/react-helmet-outputting-empty-strings-on-server-side
-const {Helmet} = require( require('path').join(process.env.BUILDPATH, "/node_modules/react-helmet") )
+const { Helmet } = require(require("path").join(
+  process.env.BUILDPATH,
+  "/node_modules/react-helmet"
+));
 
-const jsonStringify = require('json-stringify-safe')
+const jsonStringify = require("json-stringify-safe");
 // const bundle = require('./bundle')
 // var bundleInfo = false
+var ssrCrashWarned = false;
 
-async function generateComponent(req, res, componentPath, bundlePath, basePath, bundleInfo){
-
+async function generateComponent(
+  req,
+  res,
+  componentPath,
+  bundlePath,
+  basePath,
+  bundleInfo
+) {
   // invalidate node module's cache in dev mode
   // TODO: invalidate nested requires too.
   // TODO: only invalidate if file has changed and not on each refresh
   if (ISDEV) {
-    delete require.cache[require.resolve(componentPath)]
+    delete require.cache[require.resolve(componentPath)];
     //console.log("CACHE", require.cache)
     //delete require.cache
   }
 
-  try{
-  var App = require(componentPath)
-  }
-  catch(e){
-    console.log(e)
+  try {
+    var App = require(componentPath);
+  } catch (e) {
+    console.log(e);
   }
 
-  App = (App && App.default)?App.default : App // cater export default class...
-  if (!App){
-    // component failed to load or was not exported. 
-    if (bundleInfo && bundleInfo.js){
+  App = App && App.default ? App.default : App; // cater export default class...
+  if (!App) {
+    // component failed to load or was not exported.
+
+    if (bundleInfo && bundleInfo.js) {
       // atleast we have a bundle. Disable SSR for this endpoint.
-      console.warn(`\n\n⚠️ SSR didn't work for ${basePath}. Some component might not be SSR compatible.`)
+      if (!ssrCrashWarned)
+        console.warn(
+          `\n\n⚠️ SSR didn't work for ${basePath}. Some component might not be SSR compatible.`
+        );
+      ssrCrashWarned = true;
       var markup = `<!DOCTYPE html>
         <html>
           <head>
             <meta charset="utf-8"/>
-            ${(bundleInfo.css?`<link rel="stylesheet" href="/${bundleInfo.css}">`:"")}
+            ${
+              bundleInfo.css
+                ? `<link rel="stylesheet" href="/${bundleInfo.css}">`
+                : ""
+            }
           </head>
           <body>
             <div id="_react_root"></div>
             <script id='initial_props' type='application/json'>{}</script>
             <script src="/${bundleInfo.js}"></script>
           </body>
-        </html>`
+        </html>`;
 
-        res.write(markup)
-        res.end()
+      res.write(markup);
+      res.end();
+    } else {
+      throw new Error(
+        "Could not render this page. Did you forget to export? See logs for more info."
+      );
     }
-    else{
-      throw new Error("Could not render this page. Did you forget to export? See logs for more info.")
-    }
-  }
-  else{
-    var meta = App.meta || {}
-    var props = {user: req.user, url: {query: req.query, params: req.params}}
-    debug("App", typeof App.getInitialProps === "function")
-    if (App && App.getInitialProps && typeof App.getInitialProps === "function"){
-      try{
-        var newProps = await App.getInitialProps({req, ...props}) || {}
-        props = {...props, ...newProps}
-      }
-      catch(e){
-        debug("ERROR::getInitialProps", e)
+  } else {
+    var meta = App.meta || {};
+    var props = {
+      user: req.user,
+      url: { query: req.query, params: req.params }
+    };
+    debug("App", typeof App.getInitialProps === "function");
+    if (
+      App &&
+      App.getInitialProps &&
+      typeof App.getInitialProps === "function"
+    ) {
+      try {
+        var newProps = (await App.getInitialProps({ req, ...props })) || {};
+        props = { ...props, ...newProps };
+      } catch (e) {
+        debug("ERROR::getInitialProps", e);
       }
     }
 
     const el = isAsync(App)
       ? await createAsyncElement(App, props)
-      : React.createElement(App, props)
+      : React.createElement(App, props);
 
-    const html = renderToString(el)
-    const helmet = Helmet.renderStatic()
+    const html = renderToString(el);
+    const helmet = Helmet.renderStatic();
 
-    // determine if the user has provided with <meta charset />, 
+    // determine if the user has provided with <meta charset />,
     // if not, add a default tag with utf8
-    var hasCharset = helmet.meta.toComponent().find((meta)=>{
-      return meta.props && (meta.props['charSet'] || meta.props['charset'])
-    })
-    const json = jsonStringify(props)
+    var hasCharset = helmet.meta.toComponent().find(meta => {
+      return meta.props && (meta.props["charSet"] || meta.props["charset"]);
+    });
+    const json = jsonStringify(props);
     const finalMetaTags = {
-      title: (helmet.title.toComponent()[0].props.children.length>0)? helmet.title.toString() : (meta.title?`<title>${meta.title}</title>`:"")
-    }
+      title:
+        helmet.title.toComponent()[0].props.children.length > 0
+          ? helmet.title.toString()
+          : meta.title
+          ? `<title>${meta.title}</title>`
+          : ""
+    };
     var markup = `<!DOCTYPE html>
     <html ${helmet.htmlAttributes.toString()}>
       <head>
-        ${(!hasCharset?'<meta charset="utf-8"/>':'')}
+        ${!hasCharset ? '<meta charset="utf-8"/>' : ""}
         ${finalMetaTags.title}
         ${helmet.meta.toString()}
         ${helmet.link.toString()}
-        ${(bundleInfo.css?`<link rel="stylesheet" href="/${bundleInfo.css}">`:"")}
+        ${
+          bundleInfo.css
+            ? `<link rel="stylesheet" href="/${bundleInfo.css}">`
+            : ""
+        }
       </head>
       <body ${helmet.bodyAttributes.toString()}>
         <div id="_react_root">${html}</div>
         <script id='initial_props' type='application/json'>${json}</script>
         <script src="/${bundleInfo.js}"></script>
       </body>
-    </html>`
+    </html>`;
 
-    res.write(markup)
-    res.end()
+    res.write(markup);
+    res.end();
   }
-  
 }
 
+const isAsync = fn => fn.constructor.name === "AsyncFunction";
 
-const isAsync = fn => fn.constructor.name === 'AsyncFunction'
+const createAsyncElement = async (Component, props) => await Component(props);
 
-const createAsyncElement = async (Component, props) =>
-  await Component(props)
-
-
-module.exports = generateComponent
+module.exports = generateComponent;
 // module.exports = async (req, res, componentPath, bundlePath, basePath) => {
 //   var fullBundlePath = path.join(process.env.BUILDPATH, bundlePath)
 
@@ -136,7 +163,7 @@ module.exports = generateComponent
 //   if (ISDEV) {
 //     delete require.cache[require.resolve(componentPath)]
 //   }
-  
+
 //   if (!bundleInfo) {
 //     if (!fs.existsSync(fullBundlePath) || ISDEV) {
 //       webpackVars = await bundle(componentPath, fullBundlePath, basePath, bundlePath)
