087d0e4a1cfc7f70ca394ed73e35430f3f16f1c6
Asad Memon <asad@remoteinterview.io>
2019-04-05 15:29:35 -0700

    skip deletion of pkg.lock file in tmp

--- packages/core/lib/builder/cloneAndWatch.js
+++ packages/core/lib/builder/cloneAndWatch.js
@@ -86,6 +86,8 @@ module.exports = async (options, onWatchUpdate) => {
     var paths = [path.join(target, "/**"), "!" + target];
     if (!options.cleanModules) {
       paths.push("!" + path.join(target, "/node_modules/**"));
+      paths.push("!" + path.join(target, "/package-lock.json"));
+      paths.push("!" + path.join(target, "/yarn.lock"));
     }
     // if running in prod mode, also avoid deleting builds.
     //if (!ISDEV) paths.push('!'+path.join(target, '/zero-builds/**') )
