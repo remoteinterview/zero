643064b7610e224b65c5f74dc20422566670df1b
Asad Memon <asad@remoteinterview.io>
2019-04-17 13:39:38 -0700

    update python shim to prefer python3

--- packages/handler-python/handler.js
+++ packages/handler-python/handler.js
@@ -4,8 +4,10 @@ const path = require("path");
 const fs = require("fs");
 const which = require("which");
 const pythonExists = which.sync("python", { nothrow: true });
+const python3Exists = which.sync("python3", { nothrow: true });
 
-module.exports = async (req, res, file, bundlePath) => {
-  if (!pythonExists) throw new Error("No 'python' found in the PATH.");
+module.exports = async (req, res, file, bundlePath, basePath, bundleInfo) => {
+  const pythonExe = python3Exists || pythonExists;
+  if (!pythonExe) throw new Error("No 'python' found in the PATH.");
   res.send("Python handler is coming soon.");
 };
