75719961f11d73039c78d951df223491bb6232c2
Asad Memon <asad@remoteinterview.io>
2019-08-22 15:32:06 -0700

    error on missing export on js handler

--- packages/handler-js/handler.js
+++ packages/handler-js/handler.js
@@ -3,5 +3,11 @@ module.exports = (req, res, file, bundlePath, basePath, bundleInfo) => {
   if (!bundleInfo || !bundleInfo.js) return res.sendStatus(500);
   var func = require(path.join(process.env.BUILDPATH, bundleInfo.js));
   func = func && func.default ? func.default : func; // cater export default function...
+  if (!func || typeof func !== "function") {
+    console.log(
+      `❓ Did you forget to export handler in ${path.basename(file)}?`
+    );
+    return res.sendStatus(500);
+  }
   func(req, res);
 };
